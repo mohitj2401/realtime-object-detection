e567dcc87a92309d1b9aed9109fc2a05d95dd2ad
Mohit jain <mohit.edu7895@gmail.com>
2023-02-25 13:56:57 +0530

    Realtime object detection project completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Object Detection TFLite',
       theme: ThemeData(
         primarySwatch: Colors.blue,
